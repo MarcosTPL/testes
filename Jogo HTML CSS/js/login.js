98011f9354a02808c8aa20f52097e825ff1da945
MarcosTPL <62801755+MarcosTPL@users.noreply.github.com>
2021-01-20 17:47:56 -0300

    Add files via upload

---
+++ Jogo HTML CSS/js/login.js
@@ -0,0 +1,12 @@
+/*
+function abreLinkLogin(){
+    window.location="login.html";
+}
+*/
+function abreLinkRegister(){
+    window.location="register.html";
+}
+function abreLinkMenu(){
+    window.location="menu.html";
+}
+
